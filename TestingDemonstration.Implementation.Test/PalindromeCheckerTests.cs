28f20700ea0647384aa16455fe3282f374c5a101
David Padilla <davidpadilla.f22@gmail.com>
2022-02-24 13:00:31 -0500

    create palindrome checker

---
+++ TestingDemonstration.Implementation.Test/PalindromeCheckerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xunit;
+
+namespace TestingDemonstration.Implementation.Test
+{
+    public class PalindromeCheckerTests
+    {
+        private readonly PalindromeChecker _checker;
+
+        public PalindromeCheckerTests()
+        {
+            // arrange
+            _checker = new PalindromeChecker();
+        }
+
+        [Theory]
+        [InlineData(true, "level")]
+        [InlineData(false, "car")]
+        [InlineData(true, "LEVEL")]
+        [InlineData(true, "racecar")]
+        [InlineData(true, "level level")]
+        [InlineData(true, "123level321")]
+        [InlineData(false, "")]
+        public void Check_ReturnsTrueOnlyWhenPalindrome(bool expect, string word)
+        {
+
+            // act
+            var result = _checker.Check(word);
+
+            // assert
+            Assert.Equal(expect, result);
+        }
+
+        [Fact]
+        public void Check_StringWithSpecialCharacters_ThrowsArgumentException()
+        {
+            var word = "!@#$@#$";
+            Assert.Throws<ArgumentException>(() => _checker.Check(word));
+        }
+
+
+
+    }
+}
---
+++ TestingDemonstration.Implementation/PalindromeChecker.cs
@@ -0,0 +1,16 @@
+﻿namespace TestingDemonstration.Implementation
+{
+    public class PalindromeChecker
+    {
+        public bool Check(string word)
+        {
+            if (string.IsNullOrEmpty(word)) return false;
+
+            var areAllLetters = word.All(c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c));
+            if (!areAllLetters) throw new ArgumentException();
+
+            var reversed = new string(word.Reverse().ToArray());
+            return reversed.Equals(word);
+        }
+    }
+}
